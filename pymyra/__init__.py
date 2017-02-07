27ec117488105b3547af4b674b0997f13de0d594
viksit <viksit@gmail.com>
2017-02-06 19:14:26 -0800

    bump version

--- pymyra/__init__.py
+++ pymyra/__init__.py
@@ -6,6 +6,6 @@ from . import api
 __author__ = 'Myra Labs, Inc.'
 __copyright__ = 'Copyright 2016 Myra Labs, Inc.'
 
-__version_info__ = ('1', '0', '4')
+__version_info__ = ('1', '0', '5')
 __version__ = '.'.join(__version_info__)
 __short_version__ = '.'.join(__version_info__)
